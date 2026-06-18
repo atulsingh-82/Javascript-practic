bb3241e8c360c0fe388604520478e5c34a9f1f0e
Atul Prasad <atulsingh55199@gmail.com>
2026-06-18 13:11:13 +0530

    Add files via upload

---
+++ 04_operators_2.js
@@ -0,0 +1,41 @@
+// Comparision Operators
+a = 5;
+b = 5;
+
+console.log("a = 5 and b = 5")
+console.log(a==b);  //equal to
+console.log(a!=b);  //not equal to 
+console.log(a===b); //equal value and type
+console.log(a!==b); //not ""
+console.log(a>b);   //greator than
+console.log(a<b);   //less than
+console.log(a<=b);  //less than or equal than
+
+a = 5;
+b = 7;
+console.log("a = 5  and b = 7")
+console.log(a==b);  //equal to
+console.log(a!=b);  //not equal to 
+console.log(a===b); //equal value and type
+console.log(a!==b); //not ""
+console.log(a>b);   //greator than
+console.log(a<b);   //less than
+console.log(a<=b);  //less than or equal than
+
+// Logical Operators
+
+// locical AND && [jub dono condition true ho tub hi kaam krta h]
+let age = 17;
+let height = 5;
+
+console.log("age is 18+ and height is 5",age >=18 && height === 5);
+
+// Logical OR || [Dono mai se koe bhi true aaya to true output aayga wrna false]
+let age2 = 16;
+let height2 = 6;
+console.log("age 18+ and height=5", age2 >=18 || height2 === 7);
+ 
+// Logical NOT ! [Result ko ulta kr deta hai]
+let age3 = 18;
+let height3 = 8;
+console.log( age3 >= 18 && height3 !== 8 );
