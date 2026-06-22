d6c7bc8dc6aebe5e7792f50820d65c706d45ad8a
Atul Prasad <atulsingh55199@gmail.com>
2026-06-22 11:09:09 +0530

    Add files via upload

---
+++ 06_loops_2.js
@@ -0,0 +1,17 @@
+// while loops
+let k=1;
+while(k <= 10);{
+    console.log("singh is king")
+    k++;
+}
+//Do While loop
+let x=5
+do{console.log("singh");
+    x++;
+}while(x <=6);
+
+//for of loops
+let str="atul";
+for (let i of str){
+    console.log("i=",i);
+}
