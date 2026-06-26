230f93967123eb964d8272d9bd696047573902f5
Atul Prasad <atulsingh55199@gmail.com>
2026-06-26 23:16:39 +0530

    Add files via upload

---
+++ 08_num&Maths.js
@@ -0,0 +1,15 @@
+//maths in objects
+
+/*const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance);*/
+
+//to string
+let n = 100;
+console.log(n.toString());
+
+// to fixed()
+let n1 = 12.34533;
+console.log(n1.tofixed(2));
