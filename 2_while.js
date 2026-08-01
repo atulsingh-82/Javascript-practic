d239ade66beae73b4d04e54b1e89436afa5d8e17
Atul Prasad <atulsingh55199@gmail.com>
2026-08-01 13:17:28 +0530

    Add files via upload

---
+++ 2_while.js
@@ -0,0 +1,33 @@
+// +++++++ while loop ++++++++
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index= ${index}`);
+//     index = index+2
+// }
+
+//+++++++++  Num add krna ++++++++++ 
+let num = 1
+while(num <=30){
+    console.log(num);
+    num = num+1
+}
+let index = 0
+while(index <= 18){
+    console.log(`hii ${index}`);
+    index = index+1
+}
+// +++++++++ Array ++++++++
+let myArray =["ironman","batman","superman"];
+ let arr = 0
+while(arr <= myArray.length){
+    console.log(myArray[arr]);
+    arr = arr + 1;
+}
+
+// +++++++++ Do while loop +++++++++
+let score = 20
+do {
+    console.log(score);
+    score ++
+} while (score <= 15);
